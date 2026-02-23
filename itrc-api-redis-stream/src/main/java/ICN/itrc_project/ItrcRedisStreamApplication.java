715b2d1eb297f339caa91d10a27d872bb7ea1e94
bricksky <skydh1214@naver.com>
2026-02-23 17:44:26 +0900

    feat: itrc-api-redis-stream 프로젝트 추가 - 스프링 부트 애플리케이션 초기화 및 메인 클래스 추가 - 의존성 설정: 웹, Redis, Actuator, Prometheus 연동 - 테스트 클래스(ItrcApiRedisStreamApplicationTests) 생성 - application.properties에 기본 설정 추가

---
+++ itrc-api-redis-stream/src/main/java/ICN/itrc_project/ItrcRedisStreamApplication.java
@@ -0,0 +1,13 @@
+package ICN.itrc_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItrcRedisStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItrcRedisStreamApplication.class, args);
+    }
+
+}
---
+++ itrc-api-redis-stream/src/test/java/ICN/itrc_project/ItrcApiRedisStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package ICN.itrc_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItrcApiRedisStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
